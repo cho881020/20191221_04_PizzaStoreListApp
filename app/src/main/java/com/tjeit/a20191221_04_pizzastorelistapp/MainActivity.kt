0f54cfce2b4800b7c2a97b1bf7364c36432681e2
cho881020 <kj_cho@idealidea.co.kr>
2019-12-21 15:37:19 +0900

    메인액티비티에 피자헛 목록으로 추가

--- app/src/main/java/com/tjeit/a20191221_04_pizzastorelistapp/MainActivity.kt
+++ app/src/main/java/com/tjeit/a20191221_04_pizzastorelistapp/MainActivity.kt
@@ -2,9 +2,11 @@ package com.tjeit.a20191221_04_pizzastorelistapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tjeit.a20191221_04_pizzastorelistapp.datas.PizzaStore
 
 class MainActivity : BaseActivity() {
 
+    var pizzaStoreList = ArrayList<PizzaStore>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +20,10 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        addPizzaStores()
+    }
 
+    fun addPizzaStores() {
+        pizzaStoreList.add(PizzaStore("피자헛", "010-5214-6378", "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FnkQca%2FbtqwVT4rrZo%2FymhFqW9uRbzrmZTxUU1QC1%2Fimg.jpg"))
     }
 }
