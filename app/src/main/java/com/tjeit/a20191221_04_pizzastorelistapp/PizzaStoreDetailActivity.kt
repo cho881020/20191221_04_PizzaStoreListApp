c849183fe47c34c066bcb070ce187dff23823803
cho881020 <kj_cho@idealidea.co.kr>
2019-12-21 17:01:08 +0900

    가게 상세 화면 초안 작성

---
+++ app/src/main/java/com/tjeit/a20191221_04_pizzastorelistapp/PizzaStoreDetailActivity.kt
@@ -0,0 +1,23 @@
+package com.tjeit.a20191221_04_pizzastorelistapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PizzaStoreDetailActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pizza_store_detail)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
